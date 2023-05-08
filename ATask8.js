f9ea56593b62cd105ebb663474360c1409be616e
Abhay955528 <pdiwana608@gmail.com>
2023-05-08 18:13:39 +0530

    Intro_to_Middlewares

--- ATask8.js
+++ ATask8.js
@@ -1,15 +1,22 @@
 const http = require("http");
+const bodyParser = require('body-parser');
 
 const express = require('express');
 
 const app = express();
 
-app.use((req,res,next)=>{
-  console.log('In the middleware');
-  next();// Allow the reuest to continue to the next middleware in line
+app.use(bodyParser.urlencoded({extenden: false}));
+
+app.use('/add-product',(req,res,next)=>{
+  res.send('<form action="/product" method="POST"><input type="text" name="tittle"><button type="submit">Add Product</button></form>')
 });
-app.use((req,res,next)=>{
-  console.log('In another middleware');
+
+app.post('/product',(req,res,next)=>{
+ console.log(req.body);
+  res.redirect('/');
+})
+
+app.use('/',(req,res,next)=>{
   res.send('<h1>Hellow form Express</h1>')
 });
 
